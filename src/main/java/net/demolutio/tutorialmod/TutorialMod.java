057a754778d58a0e16075ce1ed1ae5859b34bc3c
Luis Felipe de Azambuja Feyh <luisfelipe@feyh.com.br>
2026-04-20 09:58:08 -0300

    Add alexandrite items

--- src/main/java/net/demolutio/tutorialmod/TutorialMod.java
+++ src/main/java/net/demolutio/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.demolutio.tutorialmod;
 
 import com.mojang.logging.LogUtils;
 import net.demolutio.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -46,6 +47,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
